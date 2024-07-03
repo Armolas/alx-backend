e05b2dc2bb7358db0a158695ea1f341c1e0db7ad
Armolas <armolas06@gmail.com>
2024-07-03 15:13:00 +0100

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''babel flask'''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    '''The home page'''
+    return render_template('1-index.html')
