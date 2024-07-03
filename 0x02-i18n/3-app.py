3d3a94d0fd72d54012fa3aad51f30024dc8d9028
Armolas <armolas06@gmail.com>
2024-07-03 15:41:04 +0100

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''babel flask'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    '''The configuration class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''gets the language'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    '''The home page'''
+    return render_template('2-index.html')
