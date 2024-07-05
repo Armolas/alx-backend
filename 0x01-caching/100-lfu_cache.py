34b0ff74e9f3add8d8fe2958e9031ef8dc2aaf05
Armolas <armolas06@gmail.com>
2024-07-05 11:54:49 +0100

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -12,7 +12,8 @@ class LFUCache(BaseCaching):
         super().__init__()
         self.frequency = {}  # Tracks the frequency of access for each key
         self.order = {}  # Tracks the order of access for each key
-        self.access_count = 0  # Global access counter to implement LRU within the same frequency
+        self.access_count = 0
+        # Global access counter to implement LRU within the same frequency
 
     def put(self, key, item):
         '''inserts an object in the cache data'''
@@ -32,13 +33,14 @@ class LFUCache(BaseCaching):
             # Find the least frequently used keys
             min_freq = min(self.frequency.values())
             lfu_keys = [k for k, v in self.frequency.items() if v == min_freq]
-            
-            # If there's more than one LFU key, apply LRU to choose the eviction candidate
+
+            # If there's more than one LFU key,
+            # apply LRU to choose the eviction candidate
             if len(lfu_keys) > 1:
                 lru_key = min(lfu_keys, key=lambda k: self.order[k])
             else:
                 lru_key = lfu_keys[0]
-                
+
             # Evict the selected key
             del self.cache_data[lru_key]
             del self.frequency[lru_key]
